675372a98211c20b691b7e2b31fc142c5262ce82
zeroengineer <iamdevangcv@gmail.com>
2026-04-10 08:31:31 +0530

    first iteration changes

--- vite.config.ts
+++ vite.config.ts
@@ -12,11 +12,11 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://127.0.0.1:3001',
         changeOrigin: true,
       },
       '/ws': {
-        target: 'http://localhost:3001',
+        target: 'http://127.0.0.1:3001',
         changeOrigin: true,
         ws: true,
       },
